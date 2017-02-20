52364b0b647065bf818f2a29ef8d50ee0c11b591
Aaron Johnson <johnsaar@oregonstate.edu>
2017-02-19 19:33:08 -0500

    Add comments to Tool
    Tool.cpp and Tool.hpp were commented fully

--- src/tool.cpp
+++ src/tool.cpp
@@ -1,15 +1,27 @@
+
+/******************************************************************************
+** Project Group: Group 6
+** Author: Aaron Johnson
+** Date: 19 Feb 2017
+** Filename: tool.cpp
+** Description: Implementation file for the Tool class. It contains default
+				behavior that will be inherited by classes derived from Tool.
+******************************************************************************/
 #include "tool.hpp"
 
+//Default constructor
 Tool::Tool()
 {
 	this->strength = 1;
 }
 
+//Parameterized constructor
 Tool::Tool(int strength)
 {
 	this->strength = strength;
 }
 
+//Destructor
 Tool::~Tool()
 {
 }
--- src/tool.hpp
+++ src/tool.hpp
@@ -1,3 +1,14 @@
+/******************************************************************************
+** Project Group: Group 6
+** Author: Aaron Johnson
+** Date: 19 Feb 2017
+** Filename: tool.hpp
+** Description: Header file for the Tool class. The tool class contains
+				the the parent behavior for tools. It contains the parent tool
+				class that can be used to make a rock, paper, scissors game.
+				This is a pure abstract class and cannot be instantiated 
+				on its own.
+******************************************************************************/
 #ifndef TOOL_HPP
 #define TOOL_HPP
 
@@ -16,6 +27,6 @@ public:
 	void setStrength(int strength);
 	int getStrength() const;
 	char getType() const;
-	virtual Fight fight(Tool *tool) = 0;
+	virtual Fight fight(Tool *tool) = 0; //pure abstract function. Cannot instantiate tools by themselves.
 };
 #endif
